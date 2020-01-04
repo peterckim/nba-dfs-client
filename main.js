e84462feb560993771e0b20ed235a4cf3affc8b8
peterckim <peterckim96@gmail.com>
2020-01-04 10:13:20 -0500

    adding comments

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 window.onload = function() {
+  /* Begin running the code */
   start();
 };
 
+/* Initial Method */
 start = () => {
+  /* Handle Data + Render the Charts */
   getData();
+
+  /* Render Player List */
   renderList();
 };
 
+/* Initial Method to Render Player List */
 renderList = () => {
   fetchAllPlayers();
 };
 
+/* Fetch All Players from API */
 async function fetchAllPlayers() {
   const playersData = await fetch("http://localhost:5000/players");
 
   parseAllPlayerData(playersData);
 }
 
+/* Parse All Player Data to JSON */
 async function parseAllPlayerData(playersData) {
   const parsedPlayersData = await playersData.json();
 
   createAllPlayersObjects(parsedPlayersData);
 }
 
+/* Create Player Instances from Fetched Data */
 createAllPlayersObjects = data => {
   data.players.forEach(
     player =>
@@ -36,6 +45,7 @@ createAllPlayersObjects = data => {
   displayList();
 };
 
+/* Render List of Players to HTML */
 displayList = () => {
   const list = document.querySelector(".players-list");
 
@@ -44,18 +54,21 @@ displayList = () => {
   });
 };
 
+/* Get Player Data */
 async function getData() {
   const data = await fetch("http://localhost:5000/players/337");
 
   parseData(data);
 }
 
+/* Parse Player Data to JSON */
 async function parseData(data) {
   const parsedData = await data.json();
 
   createPlayer(parsedData);
 }
 
+/* Create Player Instance off Fetched Data */
 createPlayer = playerMeta => {
   const player = new Player(playerMeta);
 
@@ -64,72 +77,7 @@ createPlayer = playerMeta => {
   gatherDataPoints(player, "value");
 };
 
-calculateFanduelPoints = game => {
-  let fantasyPoints = 0;
-
-  fantasyPoints += game.points;
-  fantasyPoints += game.rebounds * 1.2;
-  fantasyPoints += game.assists * 1.5;
-  fantasyPoints += game.steals * 3;
-  fantasyPoints += game.blocks * 3;
-  fantasyPoints -= game.turnovers;
-
-  return fantasyPoints;
-};
-
-calculateValue = game => {
-  let points = calculateFanduelPoints(game);
-
-  return (points / game.price) * 1000;
-};
-
-parseDate = date => {
-  let month = date.getMonth();
-  let monthWord = "";
-
-  switch (month) {
-    case 0:
-      monthWord = "Jan";
-      break;
-    case 1:
-      monthWord = "Feb";
-      break;
-    case 2:
-      monthWord = "Mar";
-      break;
-    case 3:
-      monthWord = "Apr";
-      break;
-    case 4:
-      monthWord = "May";
-      break;
-    case 5:
-      monthWord = "June";
-      break;
-    case 6:
-      monthWord = "July";
-      break;
-    case 7:
-      monthWord = "Aug";
-      break;
-    case 8:
-      monthWord = "Sep";
-      break;
-    case 9:
-      monthWord = "Oct";
-      break;
-    case 10:
-      monthWord = "Nov";
-      break;
-    case 11:
-      monthWord = "Dec";
-      break;
-    default:
-      monthWord = "Jan";
-  }
-  return `${monthWord} ${date.getDate()}, ${date.getFullYear()}`;
-};
-
+/* Populate Data Points for Chart */
 gatherDataPoints = (player, type) => {
   let dataPoints = null;
   if (type == "last10") {
@@ -158,6 +106,7 @@ gatherDataPoints = (player, type) => {
   populateChart(player, dataPoints, type);
 };
 
+/* Create the Charts */
 populateChart = (player, data, type) => {
   let chart = null;
   if (type == "last10") {
@@ -207,6 +156,76 @@ populateChart = (player, data, type) => {
   renderChart(chart);
 };
 
+/* Render Canvas Chart to HTML */
 renderChart = chart => {
   chart.render();
+};
+
+/* Helper Method to Calculate Fanduel Points */
+calculateFanduelPoints = game => {
+  let fantasyPoints = 0;
+
+  fantasyPoints += game.points;
+  fantasyPoints += game.rebounds * 1.2;
+  fantasyPoints += game.assists * 1.5;
+  fantasyPoints += game.steals * 3;
+  fantasyPoints += game.blocks * 3;
+  fantasyPoints -= game.turnovers;
+
+  return fantasyPoints;
+};
+
+/* Helper Method to Calculate Value Returned of Player */
+calculateValue = game => {
+  let points = calculateFanduelPoints(game);
+
+  return (points / game.price) * 1000;
+};
+
+/* Helper Method to Parse Date to more Friendly Format */
+parseDate = date => {
+  let month = date.getMonth();
+  let monthWord = "";
+
+  switch (month) {
+    case 0:
+      monthWord = "Jan";
+      break;
+    case 1:
+      monthWord = "Feb";
+      break;
+    case 2:
+      monthWord = "Mar";
+      break;
+    case 3:
+      monthWord = "Apr";
+      break;
+    case 4:
+      monthWord = "May";
+      break;
+    case 5:
+      monthWord = "June";
+      break;
+    case 6:
+      monthWord = "July";
+      break;
+    case 7:
+      monthWord = "Aug";
+      break;
+    case 8:
+      monthWord = "Sep";
+      break;
+    case 9:
+      monthWord = "Oct";
+      break;
+    case 10:
+      monthWord = "Nov";
+      break;
+    case 11:
+      monthWord = "Dec";
+      break;
+    default:
+      monthWord = "Jan";
+  }
+  return `${monthWord} ${date.getDate()}, ${date.getFullYear()}`;
 };
--- player.js
+++ player.js
@@ -1,5 +1,9 @@
+/* Player Object */
 class Player {
+  /* Array of ALL Players */
   static all = [];
+
+  /* Constructor Method */
   constructor(args) {
     this.id = args.id;
     this.name = args.name;
@@ -8,28 +12,29 @@ class Player {
     Player.all.push(this);
   }
 
+  /* Getter Method for Player Name */
   getName = () => {
     return this.name;
   };
 
+  /* Getter Method for Player Games */
   getGames = () => {
     return this.games.reverse();
   };
 
-  addGame = game => {
-    this.games.push(game);
-  };
-
+  /* Static Method to Get ALL Players Instances */
   static getAll = () => {
     return Player.all;
   };
 
+  /* Static Method to Get ALL Players with a Certain Position */
   static getPosition = position => {
     return Player.all.filter(el => {
       return el.position === position;
     });
   };
 
+  /* Getter Method to Retrieve ALL Games Against a Certain Team */
   getGamesAgainst = team => {
     return this.games.filter(el => {
       return el.opponent === team;
